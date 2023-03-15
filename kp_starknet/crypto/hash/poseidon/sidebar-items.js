c5972b1090eaa8ccccb210966775c4b749310858
EvolveArt <EvolveArt@users.noreply.github.com>
2023-03-15 20:17:21 +0000

    deploy: f783c8f981e6aedda4c7851ad3e568c83e5ed8b2

---
+++ kp_starknet/crypto/hash/poseidon/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["hash","The Poseidon hash function."]],"struct":[["PoseidonHasher","The poseidon hasher."]]};
---
+++ kp_starknet/traits/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":[["hash","Define traits related to hash functions. Traits for hashing."],["transaction","Define traits related to transaction."]]};
